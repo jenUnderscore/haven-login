7fa7e0320b2cedaa4bb3b4b69cad3813a6c6bbcc
Jen Hood <jhood@underscoresolutions.com>
2023-02-26 20:08:28 -0500

    update version

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 #==  Current plugin version.
-define( 'HAVEN_LOGIN_VERSION', '1.0.2' );
+define( 'HAVEN_LOGIN_VERSION', '1.0.3' );
 
-define( 'HAVEN_DEPENDENCY_VERSION', '1.0.2' );
+define( 'HAVEN_DEPENDENCY_VERSION', '1.0.3' );
 
 define( 'HAVEN_LOGIN_DEBUG', false );
