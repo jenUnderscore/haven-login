81c16842c3a930f30d008d65eaefb025a026b25b
Jen Hood <jhood@underscoresolutions.com>
2023-03-01 09:17:54 -0500

    version update

--- haven-login.php
+++ haven-login.php
@@ -3,7 +3,7 @@
 Plugin Name: Haven Login
 Description: Plugin for adding login button for dashboard
 Author: Haven Destinations
-Version: 1.0.1
+Version: 1.0.0
 Author URI: http://havendestinations.ca
 */
 
